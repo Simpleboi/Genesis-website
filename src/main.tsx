225a25891d2ff9aa83cb6b60f49339eb09ee0b2f
Biggie Cheese <Jv3studios@gmail.com>
2025-03-12 21:58:25 -0500

    wokring on transferring from css to scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "../src/styles/index.scss";
 import { BrowserRouter } from "react-router-dom";
-
 import { TempoDevtools } from "tempo-devtools";
 TempoDevtools.init();
 
